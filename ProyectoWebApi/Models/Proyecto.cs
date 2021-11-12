d8c2bd6377cf1ae235a38d19dcfcabce3f8e2a7d
Sthepanie <sthepaniear@1gmail.com>
2021-11-12 11:38:43 -0400

    Modelo proyecto creado

---
+++ ProyectoWebApi/Models/Proyecto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoWebApi.Models
+{
+    public class Proyecto
+    {
+        [Key]
+        public string Usuario { get; set; } //POSIBLE CAMBIO
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Contraseña { get; set; }//POSIBLE CAMBIO
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Sintoma { get; set; } //POSIBLE CAMBIO
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+    }
+}
